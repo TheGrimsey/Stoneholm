497bea529ddc10d94009d8f0eb8fb1d07fd6aa4c
TheGrimsey <2411768-TheGrimsey@users.noreply.gitlab.com>
2021-02-27 00:17:04 +0100

    More balancing and minor fixes to structures.

--- src/main/java/net/thegrimsey/stoneholm/Stoneholm.java
+++ src/main/java/net/thegrimsey/stoneholm/Stoneholm.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
 
 public class Stoneholm implements ModInitializer {
@@ -17,7 +18,8 @@ public class Stoneholm implements ModInitializer {
 
 		BiomeModifications.create(new Identifier(MODID, "underground_village"))
 				.add(ModificationPhase.ADDITIONS,
-						BiomeSelectors.includeByKey(BiomeKeys.DARK_FOREST, BiomeKeys.FOREST, BiomeKeys.BIRCH_FOREST, BiomeKeys.JUNGLE, BiomeKeys.TALL_BIRCH_FOREST, BiomeKeys.SAVANNA, BiomeKeys.PLAINS, BiomeKeys.FLOWER_FOREST, BiomeKeys.TAIGA),
+						BiomeSelectors.includeByKey(BiomeKeys.DARK_FOREST, BiomeKeys.FOREST, BiomeKeys.BIRCH_FOREST, BiomeKeys.JUNGLE, BiomeKeys.TALL_BIRCH_FOREST, BiomeKeys.TAIGA, BiomeKeys.FLOWER_FOREST, BiomeKeys.GIANT_TREE_TAIGA, BiomeKeys.GIANT_SPRUCE_TAIGA,
+								BiomeKeys.SAVANNA, BiomeKeys.PLAINS, BiomeKeys.SNOWY_TUNDRA),
 						context -> {
 							context.getGenerationSettings().addBuiltInStructure(SHConfiguredStructures.CONFIGURED_UNDERGROUND_VILLAGE);
 						});
