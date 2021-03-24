0dc71362c29a642bc3c89cf5200a8d305b147bc7
Grim <TheGrimsey@users.noreply.github.com>
2021-03-24 14:19:28 +0100

    V1.0.1
    - Slight adjustments to bedroom loot_table.
    - Decrease default VILLAGE_MAX_DISTANCE by 1.
    - Minor changes to structures.
    - Bedrooms now spawn in abandoned sections.

--- src/main/java/net/thegrimsey/stoneholm/SHConfig.java
+++ src/main/java/net/thegrimsey/stoneholm/SHConfig.java
@@ -8,7 +8,7 @@ public class SHConfig implements ConfigData {
     // How many iterations of jigsaw generation we do when generating the village.
     public int VILLAGE_SIZE = 12;
     // Max distance in chunks between villages.
-    public int VILLAGE_MAX_DISTANCE = 35;
+    public int VILLAGE_MAX_DISTANCE = 34;
     // Minimum distance in chunks between villages.
     public int VILLAGE_MIN_DISTANCE = 18;
     // How far above sea level a village may be. This is used to prevent pieces from generating in the air in less even terrains.
